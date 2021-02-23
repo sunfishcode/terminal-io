441f6f8984f2fded188b07b37094d02286430777
Dan Gohman <dev@sunfishcode.online>
2021-02-23 06:49:50 -0800

    Use unsafe-io's `ReadHalf` and `WriteHalf` in config.rs.

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,10 @@
 use crate::TerminalColorSupport;
 use duplex::Duplex;
-use unsafe_io::{AsUnsafeHandle, AsUnsafeReadWriteHandle};
+use unsafe_io::{AsUnsafeHandle, AsUnsafeReadWriteHandle, ReadHalf, WriteHalf};
 #[cfg(not(windows))]
-use {
-    std::mem::MaybeUninit,
-    unsafe_io::os::posish::{AsRawFd, RawFd},
-};
+use {std::mem::MaybeUninit, unsafe_io::os::posish::AsRawFd};
 #[cfg(windows)]
-use {
-    std::os::windows::io::AsRawHandle,
-    unsafe_io::os::windows::{AsRawHandleOrSocket, RawHandleOrSocket},
-};
+use {std::os::windows::io::AsRawHandle, unsafe_io::os::windows::AsRawHandleOrSocket};
 
 #[derive(Debug)]
 pub(crate) struct ReadConfig {
@@ -31,93 +25,40 @@ impl Default for ReadConfig {
     }
 }
 
-pub(crate) fn detect_read_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<ReadConfig> {
-    #[cfg(not(windows))]
-    unsafe {
-        _detect_read_config(handle.as_unsafe_handle().as_raw_fd())
-    }
-
-    #[cfg(windows)]
-    unsafe {
-        _detect_read_config(handle.as_unsafe_handle().as_raw_handle_or_socket())
-    }
-}
-
-pub(crate) fn detect_write_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<WriteConfig> {
-    #[cfg(not(windows))]
-    unsafe {
-        _detect_write_config(handle.as_unsafe_handle().as_raw_fd())
-    }
-
-    #[cfg(windows)]
-    unsafe {
-        _detect_write_config(handle.as_unsafe_handle().as_raw_handle_or_socket())
-    }
-}
-
 pub(crate) fn detect_read_write_config<AUH: Duplex + AsUnsafeReadWriteHandle>(
     handle: &AUH,
 ) -> (Option<ReadConfig>, Option<WriteConfig>) {
-    let unsafe_read_handle = handle.as_unsafe_read_handle();
-    let unsafe_write_handle = handle.as_unsafe_read_handle();
+    let read_half = ReadHalf(handle);
+    let write_half = WriteHalf(handle);
 
-    let (read_config, write_config) = if unsafe { unsafe_write_handle.eq(unsafe_read_handle) } {
-        #[cfg(not(windows))]
-        let read_config = unsafe { _detect_read_config(unsafe_read_handle.as_raw_fd()) };
-
-        #[cfg(windows)]
-        let read_config =
-            unsafe { _detect_read_config(unsafe_read_handle.as_raw_handle_or_socket()) };
+    let (read_config, write_config) = if write_half.eq_handle(&read_half) {
+        let read_config = detect_read_config(&read_half);
 
         let write_config = if read_config.is_some() {
             #[cfg(not(windows))]
-            unsafe {
-                Some(_detect_write_config_isatty(unsafe_write_handle.as_raw_fd()))
+            {
+                Some(detect_write_config_isatty(&write_half))
             }
 
             #[cfg(windows)]
-            unsafe {
-                let color_preference = if unsafe_write_handle
-                    .eq(std::io::stdout().as_unsafe_handle())
-                    || unsafe_write_handle.eq(std::io::stderr().as_unsafe_handle())
+            {
+                let color_preference = if write_half.eq_handle(&std::io::stdout())
+                    || write_half.eq_handle(&std::io::stderr())
                 {
                     detect_stdio_color_preference()
                 } else {
                     false
                 };
-                Some(_detect_write_config_isatty(
-                    unsafe_write_handle.as_raw_handle_or_socket(),
-                    color_preference,
-                ))
+                Some(detect_write_config_isatty(&write_half, color_preference))
             }
         } else {
-            #[cfg(not(windows))]
-            unsafe {
-                _detect_write_config(unsafe_write_handle.as_raw_fd())
-            }
-
-            #[cfg(windows)]
-            unsafe {
-                _detect_write_config(unsafe_write_handle.as_raw_handle_or_socket())
-            }
+            detect_write_config(&write_half)
         };
 
         (read_config, write_config)
     } else {
-        #[cfg(not(windows))]
-        let read_config = unsafe { _detect_read_config(unsafe_read_handle.as_raw_fd()) };
-
-        #[cfg(windows)]
-        let read_config =
-            unsafe { _detect_read_config(unsafe_read_handle.as_raw_handle_or_socket()) };
-
-        #[cfg(not(windows))]
-        let write_config = unsafe { _detect_write_config(unsafe_write_handle.as_raw_fd()) };
-
-        #[cfg(windows)]
-        let write_config =
-            unsafe { _detect_write_config(unsafe_write_handle.as_raw_handle_or_socket()) };
-
+        let read_config = detect_read_config(&read_half);
+        let write_config = detect_write_config(&write_half);
         (read_config, write_config)
     };
 
@@ -125,22 +66,28 @@ pub(crate) fn detect_read_write_config<AUH: Duplex + AsUnsafeReadWriteHandle>(
 }
 
 #[cfg(not(windows))]
-unsafe fn _detect_read_config(raw_fd: RawFd) -> Option<ReadConfig> {
+pub(crate) fn detect_read_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<ReadConfig> {
     let mut termios = MaybeUninit::<libc::termios>::uninit();
 
-    if libc::tcgetattr(raw_fd, termios.as_mut_ptr()) == 0 {
-        Some(ReadConfig {
-            line_by_line: (termios.assume_init().c_lflag & libc::ICANON) == libc::ICANON,
-        })
-    } else {
-        // `tcgetattr` fails when it's not reading from a terminal.
-        None
+    unsafe {
+        if libc::tcgetattr(handle.as_unsafe_handle().as_raw_fd(), termios.as_mut_ptr()) == 0 {
+            Some(ReadConfig {
+                line_by_line: (termios.assume_init().c_lflag & libc::ICANON) == libc::ICANON,
+            })
+        } else {
+            // `tcgetattr` fails when it's not reading from a terminal.
+            None
+        }
     }
 }
 
 #[cfg(windows)]
-unsafe fn _detect_read_config(raw_handle_or_socket: RawHandleOrSocket) -> Option<ReadConfig> {
-    let isatty = match raw_handle_or_socket.as_raw_handle() {
+pub(crate) fn detect_read_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<ReadConfig> {
+    let isatty = match handle
+        .as_unsafe_handle()
+        .as_raw_handle_or_socket()
+        .as_raw_handle()
+    {
         Some(handle) => {
             if handle == std::io::stdin().as_raw_handle() {
                 atty::is(atty::Stream::Stdin)
@@ -162,17 +109,17 @@ unsafe fn _detect_read_config(raw_handle_or_socket: RawHandleOrSocket) -> Option
 }
 
 #[cfg(not(windows))]
-unsafe fn _detect_write_config(raw_fd: RawFd) -> Option<WriteConfig> {
-    if posish::io::isatty(&raw_fd) {
-        Some(_detect_write_config_isatty(raw_fd))
+pub(crate) fn detect_write_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<WriteConfig> {
+    if posish::io::isatty(handle) {
+        Some(detect_write_config_isatty(handle))
     } else {
         None
     }
 }
 
 #[cfg(not(windows))]
-unsafe fn _detect_write_config_isatty(raw_fd: RawFd) -> WriteConfig {
-    let (color_support, color_preference) = if raw_fd == std::io::stdout().as_raw_fd() {
+fn detect_write_config_isatty<AUH: AsUnsafeHandle>(handle: &AUH) -> WriteConfig {
+    let (color_support, color_preference) = if handle.eq_handle(&std::io::stdout()) {
         let info = terminfo::Database::from_env().unwrap();
         let color_support = info.get::<terminfo::capability::MaxColors>().map_or_else(
             TerminalColorSupport::default,
@@ -202,8 +149,12 @@ unsafe fn _detect_write_config_isatty(raw_fd: RawFd) -> WriteConfig {
 }
 
 #[cfg(windows)]
-unsafe fn _detect_write_config(raw_handle_or_socket: RawHandleOrSocket) -> Option<WriteConfig> {
-    let (isatty, color_preference) = match raw_handle_or_socket.as_raw_handle() {
+pub(crate) fn detect_write_config<AUH: AsUnsafeHandle>(handle: &AUH) -> Option<WriteConfig> {
+    let (isatty, color_preference) = match handle
+        .as_unsafe_handle()
+        .as_raw_handle_or_socket()
+        .as_raw_handle()
+    {
         Some(handle) => {
             if handle == std::io::stdout().as_raw_handle() {
                 (
@@ -223,18 +174,15 @@ unsafe fn _detect_write_config(raw_handle_or_socket: RawHandleOrSocket) -> Optio
     };
 
     if isatty {
-        Some(_detect_write_config_isatty(
-            raw_handle_or_socket,
-            color_preference,
-        ))
+        Some(detect_write_config_isatty(handle, color_preference))
     } else {
         None
     }
 }
 
 #[cfg(windows)]
-unsafe fn _detect_write_config_isatty(
-    _raw_handle_or_socket: RawHandleOrSocket,
+fn detect_write_config_isatty<AUH: AsUnsafeHandle>(
+    _handle: &AUH,
     color_preference: bool,
 ) -> WriteConfig {
     // Windows supports the 24-bit escape sequence but doesn't actually
